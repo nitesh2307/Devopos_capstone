c3f3bc47a956358a6540e6896aaea30377f3266c
Nitesh2307 <nitesh2307@gmail.com>
2023-04-15 15:08:02 +0000

    with cloudformation

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ def hello():
     return "Hello Nitesh - Congratulations on completing Cloud Devops Nanodegree"
     
 if __name__=="__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8080, debug=True)
+       # app.run()
--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=nitesh2307/capstone_image1:nk1 .
+docker build --tag=nitesh2307/capstone_image2 .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,9 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 nitesh2307/capstone_image1:nk1
+docker run -p 8000:8080 nitesh2307/capstone_image2
+
+#note: - 8080 is internal docker port which was used in app.py . 8000 should be used in the browser.
+# Step 4:
+docker ps
+
